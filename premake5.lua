246a175ae2ae4b65544517512697cab5781532dd
nubDotDev <41497304+nubDotDev@users.noreply.github.com>
2019-10-17 19:10:09 -0400

    Added Premake

---
+++ premake5.lua
@@ -0,0 +1,32 @@
+project "imgui"
+	kind "StaticLib"
+	language "C"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"imgui.cpp",
+		"imgui.h",
+		"imgui_demo.cpp",
+		"imgui_draw.cpp",
+		"imgui_widgets.cpp",
+		"imgui_internal.h",
+		"imconfig.h",
+		"imstb_rectpack.h",
+		"imstb_textedit.h",
+		"imstb_truetype.h"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+		staticruntime "On"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
